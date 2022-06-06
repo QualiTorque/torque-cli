57516d881c477d4cd96403b11b9493aeb9c743e4
Dmytro Dovbii <dmytro.d@quali.com>
2022-06-06 14:33:45 +0300

    comment tests for validation

--- tests/integration_tests/int_test_temp_branch.py
+++ tests/integration_tests/int_test_temp_branch.py
@@ -43,119 +43,120 @@ class GitMagicTests(unittest.TestCase):
         os.chdir(self._cwd)
         shutil.rmtree(self._repo.working_dir, onerror=readonly_handler)
 
-    @patch("torque.utils.BlueprintRepo.is_current_branch_synced")
-    @patch.object(branch_utils, "create_remote_branch")
-    @patch("torque.models.blueprints.BlueprintsManager.validate")
-    @patch.object(branch_context, "delete_temp_remote_branch")
-    @patch("pkg_resources.get_distribution")
-    @patch("torque.shell.BootstrapHelper.get_connection_params")
-    @patch("torque.branch.branch_utils.debug_output_about_repo_examination")
-    @patch("torque.shell.exit")
-    def test_blueprint_validate_uncommitted_untracked(self, exit, ex, get_c, get_d, del_t, bp_validate, create_r, is_c):
-        # Arrange
-        bp_validate.return_value = Mock(errors="")
+    # TODO(ddovbii) Revert once validation is ready in spec2
+    # @patch("torque.utils.BlueprintRepo.is_current_branch_synced")
+    # @patch.object(branch_utils, "create_remote_branch")
+    # @patch("torque.models.blueprints.BlueprintsManager.validate")
+    # @patch.object(branch_context, "delete_temp_remote_branch")
+    # @patch("pkg_resources.get_distribution")
+    # @patch("torque.shell.BootstrapHelper.get_connection_params")
+    # @patch("torque.branch.branch_utils.debug_output_about_repo_examination")
+    # @patch("torque.shell.exit")
+    # def test_blueprint_validate_uncommitted_untracked(self, exit, ex, get_c, get_d, del_t, bp_validate, create_r, is_c):
+    #     # Arrange
+    #     bp_validate.return_value = Mock(errors="")
+    #
+    #     current_branch = self._repo.active_branch.name
+    #
+    #     achieve_dirty_and_untracked_repo(self._repo)
+    #
+    #     # is_current_branch_synced.return_value = False
+    #     for current_branch_sync_state in [True, False]:
+    #         is_c.return_value = current_branch_sync_state
+    #
+    #         # Act
+    #         sys.argv[1:] = ["--debug", "bp", "validate", "test2"]
+    #         shell.main()
+    #
+    #         # Assert
+    #         self._assert_untracked_reverted_with_untracked(1)
+    #         self._assert_dirty_state_reverted_dirty()
+    #         self._assert_branch_states_reverted(current_branch)
 
-        current_branch = self._repo.active_branch.name
-
-        achieve_dirty_and_untracked_repo(self._repo)
-
-        # is_current_branch_synced.return_value = False
-        for current_branch_sync_state in [True, False]:
-            is_c.return_value = current_branch_sync_state
-
-            # Act
-            sys.argv[1:] = ["--debug", "bp", "validate", "test2"]
-            shell.main()
-
-            # Assert
-            self._assert_untracked_reverted_with_untracked(1)
-            self._assert_dirty_state_reverted_dirty()
-            self._assert_branch_states_reverted(current_branch)
-
-    @patch("torque.utils.BlueprintRepo.is_current_branch_synced")
-    @patch.object(branch_utils, "create_remote_branch")
-    @patch("torque.models.blueprints.BlueprintsManager.validate")
-    @patch.object(branch_context, "delete_temp_remote_branch")
-    @patch("pkg_resources.get_distribution")
-    @patch("torque.shell.BootstrapHelper.get_connection_params")
-    @patch("torque.branch.branch_utils.debug_output_about_repo_examination")
-    @patch("torque.shell.exit")
-    def test_blueprint_validate_uncommitted(self, exit, examination, get_c, get_d, del_t, bp_validate, create_r, is_c):
-        # Arrange
-        bp_validate.return_value = Mock(errors="")
-
-        current_branch = self._repo.active_branch.name
-
-        make_repo_dirty(self._repo)
-
-        # is_current_branch_synced.return_value = False
-        for current_branch_sync_state in [True, False]:
-            is_c.return_value = current_branch_sync_state
-
-            # Act
-            sys.argv[1:] = ["--debug", "bp", "validate", "test2"]
-            shell.main()
-
-            # Assert
-            self._assert_untracked_reverted_without_untracked()
-            self._assert_dirty_state_reverted_dirty()
-            self._assert_branch_states_reverted(current_branch)
-
-    @patch("torque.utils.BlueprintRepo.is_current_branch_synced")
-    @patch.object(branch_utils, "create_remote_branch")
-    @patch("torque.models.blueprints.BlueprintsManager.validate")
-    @patch.object(branch_context, "delete_temp_remote_branch")
-    @patch("pkg_resources.get_distribution")
-    @patch("torque.shell.BootstrapHelper.get_connection_params")
-    @patch("torque.branch.branch_utils.debug_output_about_repo_examination")
-    @patch("torque.shell.exit")
-    def test_blueprint_validate_committed_untracked(self, exit, exam, get_c, get_d, del_t, bp_validate, create_r, is_c):
-        # Arrange
-        bp_validate.return_value = Mock(errors="")
-
-        current_branch = self._repo.active_branch.name
-
-        add_untracked(self._repo)
-
-        # is_current_branch_synced.return_value = False
-        for current_branch_sync_state in [True, False]:
-            is_c.return_value = current_branch_sync_state
-
-            # Act
-            sys.argv[1:] = ["--debug", "bp", "validate", "test2"]
-            shell.main()
-
-            # Assert
-            self._assert_untracked_reverted_with_untracked(1)
-            self._assert_dirty_state_reverted_clean()
-            self._assert_branch_states_reverted(current_branch)
-
-    @patch("torque.utils.BlueprintRepo.is_current_branch_synced")
-    @patch.object(branch_utils, "create_remote_branch")
-    @patch("torque.models.blueprints.BlueprintsManager.validate")
-    @patch.object(branch_context, "delete_temp_remote_branch")
-    @patch("pkg_resources.get_distribution")
-    @patch("torque.shell.BootstrapHelper.get_connection_params")
-    @patch("torque.branch.branch_utils.debug_output_about_repo_examination")
-    @patch("torque.shell.exit")
-    def test_blueprint_validate_committed(self, exit, exam, get_c, get_d, del_t, bp_validate, create_r, is_c):
-        # Arrange
-        bp_validate.return_value = Mock(errors="")
-
-        current_branch = self._repo.active_branch.name
-
-        # is_current_branch_synced.return_value = False
-        for current_branch_sync_state in [True, False]:
-            is_c.return_value = current_branch_sync_state
-
-            # Act
-            sys.argv[1:] = ["--debug", "bp", "validate", "test2"]
-            shell.main()
-
-            # Assert
-            self._assert_untracked_reverted_without_untracked()
-            self._assert_dirty_state_reverted_clean()
-            self._assert_branch_states_reverted(current_branch)
+    # @patch("torque.utils.BlueprintRepo.is_current_branch_synced")
+    # @patch.object(branch_utils, "create_remote_branch")
+    # @patch("torque.models.blueprints.BlueprintsManager.validate")
+    # @patch.object(branch_context, "delete_temp_remote_branch")
+    # @patch("pkg_resources.get_distribution")
+    # @patch("torque.shell.BootstrapHelper.get_connection_params")
+    # @patch("torque.branch.branch_utils.debug_output_about_repo_examination")
+    # @patch("torque.shell.exit")
+    # def test_blueprint_validate_uncommitted(self, exit, examination, get_c, get_d, del_t, bp_validate, create_r, is_c):
+    #     # Arrange
+    #     bp_validate.return_value = Mock(errors="")
+    #
+    #     current_branch = self._repo.active_branch.name
+    #
+    #     make_repo_dirty(self._repo)
+    #
+    #     # is_current_branch_synced.return_value = False
+    #     for current_branch_sync_state in [True, False]:
+    #         is_c.return_value = current_branch_sync_state
+    #
+    #         # Act
+    #         sys.argv[1:] = ["--debug", "bp", "validate", "test2"]
+    #         shell.main()
+    #
+    #         # Assert
+    #         self._assert_untracked_reverted_without_untracked()
+    #         self._assert_dirty_state_reverted_dirty()
+    #         self._assert_branch_states_reverted(current_branch)
+    #
+    # @patch("torque.utils.BlueprintRepo.is_current_branch_synced")
+    # @patch.object(branch_utils, "create_remote_branch")
+    # @patch("torque.models.blueprints.BlueprintsManager.validate")
+    # @patch.object(branch_context, "delete_temp_remote_branch")
+    # @patch("pkg_resources.get_distribution")
+    # @patch("torque.shell.BootstrapHelper.get_connection_params")
+    # @patch("torque.branch.branch_utils.debug_output_about_repo_examination")
+    # @patch("torque.shell.exit")
+    # def test_blueprint_validate_committed_untracked(self, exit, exam, get_c, get_d, del_t, bp_validate, create_r, is_c):
+    #     # Arrange
+    #     bp_validate.return_value = Mock(errors="")
+    #
+    #     current_branch = self._repo.active_branch.name
+    #
+    #     add_untracked(self._repo)
+    #
+    #     # is_current_branch_synced.return_value = False
+    #     for current_branch_sync_state in [True, False]:
+    #         is_c.return_value = current_branch_sync_state
+    #
+    #         # Act
+    #         sys.argv[1:] = ["--debug", "bp", "validate", "test2"]
+    #         shell.main()
+    #
+    #         # Assert
+    #         self._assert_untracked_reverted_with_untracked(1)
+    #         self._assert_dirty_state_reverted_clean()
+    #         self._assert_branch_states_reverted(current_branch)
+    #
+    # @patch("torque.utils.BlueprintRepo.is_current_branch_synced")
+    # @patch.object(branch_utils, "create_remote_branch")
+    # @patch("torque.models.blueprints.BlueprintsManager.validate")
+    # @patch.object(branch_context, "delete_temp_remote_branch")
+    # @patch("pkg_resources.get_distribution")
+    # @patch("torque.shell.BootstrapHelper.get_connection_params")
+    # @patch("torque.branch.branch_utils.debug_output_about_repo_examination")
+    # @patch("torque.shell.exit")
+    # def test_blueprint_validate_committed(self, exit, exam, get_c, get_d, del_t, bp_validate, create_r, is_c):
+    #     # Arrange
+    #     bp_validate.return_value = Mock(errors="")
+    #
+    #     current_branch = self._repo.active_branch.name
+    #
+    #     # is_current_branch_synced.return_value = False
+    #     for current_branch_sync_state in [True, False]:
+    #         is_c.return_value = current_branch_sync_state
+    #
+    #         # Act
+    #         sys.argv[1:] = ["--debug", "bp", "validate", "test2"]
+    #         shell.main()
+    #
+    #         # Assert
+    #         self._assert_untracked_reverted_without_untracked()
+    #         self._assert_dirty_state_reverted_clean()
+    #         self._assert_branch_states_reverted(current_branch)
 
     def _assert_dirty_state_reverted_dirty(self) -> None:
         changed_files_list = self._repo.git.diff("HEAD", name_only=True).split("\n")
