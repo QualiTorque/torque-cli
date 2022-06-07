0a154f93e90b350ca07d329596ce9961ef79b8c5
Dmytro Dovbii <dmytro.d@quali.com>
2022-06-07 15:26:52 +0300

    fix linter

--- torque/branch/branch_utils.py
+++ torque/branch/branch_utils.py
@@ -71,7 +71,7 @@ def get_and_check_folder_based_repo(blueprint_name: str) -> BlueprintRepo:
         repo = BlueprintRepo(os.getcwd())
         check_repo_for_errors(repo)
         debug_output_about_repo_examination(repo, blueprint_name)
-    except Exception as e:
+    except Exception:
         # logger.warning(f"Branch could not be identified/used from the working directory; reason: {e}.")
         raise
     return repo
